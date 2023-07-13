448f1c6d9dd9eb1ad5fb58f0561bf80ee6b03f04
段薛虎 <649953543@qq.com>
2023-07-13 10:50:53 +0800

    依赖包更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,12 @@ repositories {
 dependencies {
     //在IDE内运行的mcl添加滑块模块，请参考https://github.com/project-mirai/mirai-login-solver-selenium把版本更新为最新
     //runtimeOnly("net.mamoe:mirai-login-solver-selenium:1.0-dev-15")
-    implementation("org.jsoup:jsoup:1.15.3")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.alibaba.fastjson2:fastjson2:2.0.19")
-    implementation("com.baidu.aip:java-sdk:4.16.12")
+    implementation("org.jsoup:jsoup:1.15.4")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
+    implementation("com.alibaba.fastjson2:fastjson2:2.0.35")
+    implementation("com.baidu.aip:java-sdk:4.16.16")
     implementation("com.google.zxing:javase:3.5.1")
-    implementation("org.yaml:snakeyaml:1.33")
+    implementation("org.yaml:snakeyaml:2.0")
 
 }
 
